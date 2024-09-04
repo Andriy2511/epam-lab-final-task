94a82ef8b8865367acf59de0c9f8556bc23a1e76
Andrii Sirko <andriy251101@gmail.com>
2024-09-04 14:01:30 +0300

    Initial commit

---
+++ src/main/java/org/example/finalprojectepamlabapplication/FinalProjectEpamLabApplication.java
@@ -0,0 +1,13 @@
+package org.example.finalprojectepamlabapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectEpamLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectEpamLabApplication.class, args);
+	}
+
+}
